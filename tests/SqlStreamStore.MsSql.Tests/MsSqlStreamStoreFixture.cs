4e850046a86c76581a34ff7b7f3b0d501df0af2d
Damian Hickey <dhickey@gmail.com>
2020-05-17 22:48:39 +0200

    Dropping tables and recreating schema seems a lot more stable than trying to reset identity.

--- tests/SqlStreamStore.MsSql.Tests/MsSqlStreamStoreFixture.cs
+++ tests/SqlStreamStore.MsSql.Tests/MsSqlStreamStoreFixture.cs
@@ -2,14 +2,12 @@ namespace SqlStreamStore
 {
     using System;
     using System.Threading.Tasks;
-    using Microsoft.Data.SqlClient;
     using SqlStreamStore.Infrastructure;
     using SqlStreamStore.TestUtils.MsSql;
 
     public class MsSqlStreamStoreFixture : IStreamStoreFixture
     {
         private readonly Action _onDispose;
-        private bool _preparedPreviously;
         private readonly MsSqlStreamStoreSettings _settings;
 
         public MsSqlStreamStoreFixture(
@@ -57,42 +55,8 @@ namespace SqlStreamStore
         {
             MsSqlStreamStore = new MsSqlStreamStore(_settings);
 
+            await MsSqlStreamStore.DropAll();
             await MsSqlStreamStore.CreateSchema();
-            if (_preparedPreviously)
-            {
-                using (var connection = new SqlConnection(_settings.ConnectionString))
-                {
-                    connection.Open();
-
-                    var schema = _settings.Schema;
-
-                    var commandText = $"DELETE FROM [{schema}].[Messages]";
-                    using (var command = new SqlCommand(commandText, connection))
-                    {
-                        command.ExecuteNonQuery();
-                    }
-
-                    commandText = $"DELETE FROM [{schema}].[Streams]";
-                    using (var command = new SqlCommand(commandText, connection))
-                    {
-                        command.ExecuteNonQuery();
-                    }
-
-                    commandText = $"DBCC CHECKIDENT ('[{schema}].[Streams]', RESEED, -1);";
-                    using (var command = new SqlCommand(commandText, connection))
-                    {
-                        command.ExecuteNonQuery();
-                    }
-
-                    commandText = $"DBCC CHECKIDENT ('[{schema}].[Messages]', RESEED, -1);";
-                    using (var command = new SqlCommand(commandText, connection))
-                    {
-                        command.ExecuteNonQuery();
-                    }
-                }
-            }
-
-            _preparedPreviously = true;
         }
 
         public void Dispose()
--- tests/SqlStreamStore.MsSql.V3.Tests/MsSqlStreamStoreV3Fixture.cs
+++ tests/SqlStreamStore.MsSql.V3.Tests/MsSqlStreamStoreV3Fixture.cs
@@ -1,15 +1,14 @@
 ﻿namespace SqlStreamStore
 {
     using System;
+    using System.Threading;
     using System.Threading.Tasks;
-    using Microsoft.Data.SqlClient;
     using SqlStreamStore.Infrastructure;
     using SqlStreamStore.TestUtils.MsSql;
 
     public class MsSqlStreamStoreV3Fixture : IStreamStoreFixture
     {
         private readonly Action _onDispose;
-        private bool _preparedPreviously;
         private readonly MsSqlStreamStoreV3Settings _settings;
 
         public MsSqlStreamStoreV3Fixture(
@@ -63,43 +62,8 @@
         {
             _settings.DisableDeletionTracking = false;
             MsSqlStreamStoreV3 = new MsSqlStreamStoreV3(_settings);
-
+            await MsSqlStreamStoreV3.DropAll();
             await MsSqlStreamStoreV3.CreateSchemaIfNotExists();
-            if (_preparedPreviously)
-            { 
-                using (var connection = new SqlConnection(_settings.ConnectionString))
-                {
-                    await connection.OpenAsync();
-
-                    var schema = _settings.Schema;
-
-                    var commandText = $"DELETE FROM [{schema}].[Messages]";
-                    using(var command = new SqlCommand(commandText, connection))
-                    {
-                        await command.ExecuteNonQueryAsync();
-                    }
-
-                    commandText = $"DELETE FROM [{schema}].[Streams]";
-                    using(var command = new SqlCommand(commandText, connection))
-                    {
-                        await command.ExecuteNonQueryAsync();
-                    }
-
-                    commandText = $"DBCC CHECKIDENT ('[{schema}].[Streams]', RESEED, -1);";
-                    using(var command = new SqlCommand(commandText, connection))
-                    {
-                        await command.ExecuteNonQueryAsync();
-                    }
-
-                    commandText = $"DBCC CHECKIDENT ('[{schema}].[Messages]', RESEED, -1);";
-                    using(var command = new SqlCommand(commandText, connection))
-                    {
-                        await command.ExecuteNonQueryAsync();
-                    }
-                }
-            }
-
-            _preparedPreviously = true;
         }
     }
 }
